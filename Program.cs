c68971390ea28f8c27f5781220f31c84c36eae69
Moonyll <s.pouwel@laposte.net>
2018-12-20 08:48:29 +0100

    TP2' updated

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = new Random().Next(0, 50); // nombre aléatoire entre 0 et 50
+            int essai = 0;                    // nombre d'essais
+            bool ok = false;              // variable booléenne
+            Console.WriteLine("Veuillez deviner un nombre compris entre 0 et 50");
+            while (!ok)                   // Démarrage de la boucle while avec négation de la valeur booléenne
+            {
+                int rep = int.Parse(Console.ReadLine()); // conversion string en int et démarrage de la 1ière boucle if
+                if (rep == n) // le chiffre saisi est le bon                       
+                {
+                ok = true; // boucle while stoppée
+                }
+                else
+                {
+                if (rep < n)
+                {Console.WriteLine("Le chiffre saisi est trop petit, veuillez recommencer...");} // nombre trop petit
+                else
+                {Console.WriteLine("Le chiffre saisi est trop grand, veuillez recommencer...");} // nombre trop grand
+                }
+            essai++; // incrémentation du nombre d'essais
+            }
+            Console.WriteLine("Bravo, vous avez trouvé le bon chiffre en " + essai + " fois.");
+            Console.ReadKey();            
+        }
+    }    
+}
